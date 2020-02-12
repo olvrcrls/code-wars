7198c4ca5c8eea05b0d4c9f0c3e137557d631e78
Oli <dev.olivercarlos@gmail.com>
2020-02-12 22:38:44 +0800

    Make negative kata PHP.

---
+++ make_negative.php
@@ -0,0 +1,17 @@
+<?php
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+/*
+makeNegative(1)    // return -1
+makeNegative(-5)   // return -5
+makeNegative(0)    // return 0
+makeNegative(0.12) // return -0.12
+*/
+function makeNegative(float $num) : float {
+	// Shortest solution: return $num > 0 ? -$num : $num;
+    if ($num <= 0) {
+      return $num;
+    } else {
+      return -1 * $num;
+    }
+}
